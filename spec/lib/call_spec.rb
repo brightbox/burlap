3e07185dac14385c9a5a5cba7a99f638e0bcf08f
Caius Durling <dev@caius.name>
2011-02-03 18:38:09 +0000

    Updating spec for Burlap::Call#to_burlap

--- spec/lib/call_spec.rb
+++ spec/lib/call_spec.rb
@@ -3,7 +3,7 @@ require "spec/spec_helper"
 describe Burlap::Call do
 
   before do
-    @valid_params = {}
+    @valid_params = {:method => "updateUser"}
   end
 
   describe "#headers" do
@@ -20,8 +20,24 @@ describe Burlap::Call do
 
   describe "#method" do
     it "should be required" do
-      Burlap::Call.new(@valid_params.except(:method)).should raise_error(ArgumentError, "method is required")
+      lambda { Burlap::Call.new(@valid_params.except(:method)) }.should raise_error(ArgumentError, "method is required")
     end
+  end
+
+  describe "#to_burlap" do
+    before(:all) do
+      @call = Burlap::Call.new(:method => "updateUser", :arguments => ["one", 2, 3.0])
+      @response = @call.to_burlap
+    end
+    it "should have a burlap:call root" do
+      @response.should =~ /\A<burlap:call>/
+      @reponse.should =~ %r{</burlap:call>\z}
+    end
+    it "should have a method element"
+    it "should have three argument elements"
+    it "should have a string argument element"
+    it "should have an int argument element"
+    it "should have a double argument element"
   end
 
 end
