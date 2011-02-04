99248c1fd7b961aaabf987b287c0cc02bafc0dd4
Caius Durling <dev@caius.name>
2011-02-04 10:37:00 +0000

    Pending stuff should be pending

--- spec/lib/hash_spec.rb
+++ spec/lib/hash_spec.rb
@@ -6,14 +6,14 @@ describe Burlap::Hash do
     Burlap::Hash.ancestors.should include(Hash)
   end
 
-  it "should behave like a Hash" do
-    
-  end
+  it "should behave like a Hash"
 
   describe "#__type__" do
     it "should default to Hash" do
       Burlap::Hash.new.__type__.should == "Hash"
     end
   end
+
+  it { should respond_to(:__type__=) }
 
 end
