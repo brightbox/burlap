8ed4d253bf5ee17d23482c1e3e0a39be5c7e0a23
Caius Durling <dev@caius.name>
2011-02-04 15:08:23 +0000

    Implementing the generator spec

--- lib/burlap/generator.rb
+++ lib/burlap/generator.rb
@@ -1,35 +1,41 @@
-
 module Burlap
   class Generator
-    attr_reader :data
+    GeneratorError = Class.new(Burlap::Error)
+
+    attr_reader :root, :data
 
     def initialize data
-      raise ArgumentError, "data must be a hash" unless data.is_a?(Hash)
-      self.data = data
+      raise ArgumentError, "data is not a Hash" unless data.is_a?(::Hash)
+      raise ArgumentError, "data can only contain one root element" if data.keys.size > 1
+      self.root = data.keys.first
+      self.data = data[root]
     end
 
-    def dump
-      
+    def to_xml
+      nx = Nokogiri::XML
+      doc = nx::Document.new
+      root_node = nx::Node.new(root, doc)
+      doc.add_child root_node
+
+      data.each do |e|
+        raise GeneratorError, "Couldn't generate burlap for #{e.class.to_s} #{e.inspect}. Please mix in Burlap::Emit for support or implement :burlap_node" unless e.respond_to?(:burlap_node)
+
+        node = #if e.method(:burlap_node).arity > 0
+          e.burlap_node(root_node)
+        # else
+        #   name, contents = e.burlap_node
+        #   n = Nokogiri::XML::Node.new name, root_node.document
+        #   n.content = contents
+        #   n
+        # end
+        root_node.add_child node
+      end
+
+      root_node.to_xml
     end
 
   protected
-    attr_writer :data
+    attr_writer :root, :data
   end
 
-  
-
 end
-
-
-
-data = {"method" => "fred", "args" => }
-
-Burlap::Generator.dump(data)
-
-
-__END__
-<burlap:call>
-  <method>fred</method>
-  <string>blogs</string>
-  <int>1</int>
-</burlap:call>
