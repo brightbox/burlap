e197a8a183b9dcae588bf4073d8c6e135197b197
Caius Durling <dev@caius.name>
2011-02-09 16:58:46 +0000

    Implementing node to make the basic core extensions pass

--- lib/burlap/node.rb
+++ lib/burlap/node.rb
@@ -1,13 +1,13 @@
 module Burlap
   class Node
-    attr_accessor :name, :contents
+    attr_accessor :name, :value
 
     # name is the element name to go in the <>
-    # and content is either an array of nested nodes, or a string to be the node content
+    # and value is a string to go in the node
     def initialize params={}
       params.each do |k,v|
         meffod = :"#{k}="
-        send(meffod, v) if respond_to?(meffod)
+        send(meffod, v) #if respond_to?(meffod)
       end
     end
 
@@ -15,15 +15,11 @@ module Burlap
     def to_burlap
       doc = Nokogiri::XML::Document.new
       root = Nokogiri::XML::Node.new(self.name, doc)
-      # Would like to use respond_to?(:each), but String implements that
-      # and we don't want to call String#each
-      if self.contents.is_a?(Array)
-        root << self.contents.to_burlap
-      else
-        # Use it as this node's content
-        self.contents = self.contents
-      end
 
+      p self.value
+      root.content = self.value.to_s
+
+      root.to_xml
     end
 
   end
