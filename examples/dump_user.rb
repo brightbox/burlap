c7efac4659f4b652848c27cc61a9e63d9a39e7fe
Caius Durling <dev@caius.name>
2011-02-09 10:41:37 +0000

    More stuff

--- examples/dump_user.rb
+++ examples/dump_user.rb
@@ -3,30 +3,25 @@ require "bundler/setup"
 require "burlap"
 require "pp"
 
-data = {
-  "method" => "updateUser",
-  "arguments" => [
-    Burlap::Object.new(
-      :type => "com.sapienter.jbilling.server.user.UserWS",
-      :contents => {
-        :balanceType => nil,
-        :userName => "Caius",
-        :languageId => 1,
-        :creditCard => nil,
-        :createDatetime => Time.local(2010, 9, 11, 10, 0, 0),
-        :mainRoleId => 2,
-        :contact => Burlap::Object.new(
-          :type => "com.sapienter.jbilling.server.user.ContactWS",
-          :contents => {
-            :city => "Leeds",
-            :createDate => Time.local(2010, 9, 11, 10, 0, 0)
-          }
-        )
-      }
-    )
+data = Burlap::Call.new(
+  :method => "updateUser",
+  :arguments => [
+    :balanceType => nil,
+    :userName => "Caius",
+    :languageId => 1,
+    :creditCard => nil,
+    :createDatetime => Time.local(2010, 9, 11, 10, 0, 0),
+    :mainRoleId => 2,
+    :contact => Burlap::Hash[
+      {
+        :city => "Leeds",
+        :createDate => Time.local(2010, 9, 11, 10, 0, 0)
+      },
+      "com.sapienter.jbilling.server.user.ContactWS"
+    ]
   ]
-}
+)
 
-xml = Burlap.dump(data)
+xml = data.to_burlap
 
 pp xml
--- lib/burlap/node.rb
+++ lib/burlap/node.rb
@@ -13,18 +13,18 @@ module Burlap
 
     # Packs this object into nokogiri nodes and returns the XML as a string
     def to_burlap
+      doc = Nokogiri::XML::Document.new
+      root = Nokogiri::XML::Node.new(self.name, doc)
       # Would like to use respond_to?(:each), but String implements that
       # and we don't want to call String#each
       if self.contents.is_a?(Array)
-        result = self.contents.to_burlap
-        root << result.to_xml(doc)
+        root << self.contents.to_burlap
       else
         # Use it as this node's content
         self.contents = self.contents
       end
 
     end
-    alias_method :to_xml, :to_burlap
 
   end
 end
