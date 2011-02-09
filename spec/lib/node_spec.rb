04163c2b9244ed55df2236d754fb499b8e86764f
Caius Durling <dev@caius.name>
2011-02-09 17:22:56 +0000

    Implementing Burlap::Node#to_burlap spec

--- spec/lib/node_spec.rb
+++ spec/lib/node_spec.rb
@@ -2,4 +2,16 @@ require "spec_helper"
 
 describe Burlap::Node do
 
+  describe "#to_burlap" do
+    before :all do
+      @result = Burlap::Node.new(:name => "method", :value => "updateUser").to_burlap
+    end
+    it "should return a string" do
+      @result.should be_a_kind_of(String)
+    end
+    it "should put name in brackets" do
+      @result.should == "<method>updateUser</method>"
+    end
+  end
+
 end
