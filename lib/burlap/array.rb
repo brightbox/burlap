d2ba5eb9fb94cbe51fc54d12508c3ff418a43020
Caius Durling <dev@caius.name>
2011-02-10 14:48:23 +0000

    Sorting out Burlap::Array#to_burlap & it's spec

--- lib/burlap/array.rb
+++ lib/burlap/array.rb
@@ -1,8 +1,15 @@
 module Burlap
   class Array < ::Array
-
+    
     def to_burlap
-      list_content = [Burlap::Node.new(:name => "length", :value => self.length.to_s).to_burlap]
+      # <list><type>[string</type><length>1</length><string>Some</string></list>
+      list_content = [
+        Burlap::Node.new(:name => "type", :value => "").to_burlap,
+        Burlap::Node.new(:name => "length", :value => self.length.to_s).to_burlap
+      ]
+      list_content += self.map do |element|
+        element.to_burlap
+      end
       list_content = list_content.join("")
       Burlap::Node.new(:name => "list", :value => list_content).to_burlap
     end
--- spec/lib/array_spec.rb
+++ spec/lib/array_spec.rb
@@ -3,8 +3,9 @@ require "spec_helper"
 describe Burlap::Array do
   describe "#to_burlap" do
     before do
-      @result = Burlap::Array["some", :value].to_burlap
-      p @result
+      @mock_obj = mock(Object, :to_burlap => "<mock>my stuff here</mock>")
+      @array = Burlap::Array["some", @mock_obj]
+      @result = @array.to_burlap
       @doc = Nokogiri::XML(@result)
     end
     it "should return a string" do
@@ -12,6 +13,33 @@ describe Burlap::Array do
     end
     it "should be a list" do
       @doc.css("list").should have(1).element
+    end
+    it "should have a type attribute" do
+      type, = element_exists_with :selector => "type", :count => 1
+      type.name.should == "type"
+      type.content.should == ""
+    end
+    it "should have a length attribute" do
+      value, = element_exists_with :selector => "length", :count => 1
+      value.name.should == "length"
+      value.content.should == @array.size.to_s
+    end
+    it "should have a string attribute" do
+      str, = element_exists_with :selector => "string", :count => 1
+      str.name.should == "string"
+      str.content.should == "some"
+    end
+    it "should have a mock attribute" do
+      obj, = element_exists_with :selector => "mock", :count => 1
+      obj.name.should == "mock"
+      obj.content.should == "my stuff here"
+    end
+
+  protected
+    def element_exists_with opts={}
+      elements = @doc.css(opts[:selector])
+      elements.should have(opts[:count]).elements
+      elements
     end
   end
 end
