fb4471749731dce897cc04332f8c5fd916e06968
Caius Durling <dev@caius.name>
2011-02-10 17:44:28 +0000

    Implementing Array#to_burlap to invoke Burlap::Array

--- lib/burlap/core_ext/array.rb
+++ lib/burlap/core_ext/array.rb
@@ -1,5 +1,7 @@
 class Array
   def to_burlap
-    Burlap::Array[self].to_burlap
+    # We use the splat here so it creates an array with our elements,
+    # rather than an array containing an array of our elements
+    Burlap::Array[*to_a].to_burlap
   end
 end
--- spec/lib/core_ext/array_spec.rb
+++ spec/lib/core_ext/array_spec.rb
@@ -1,5 +1,18 @@
 require "spec_helper"
 
 describe Array do
-  # it should create Burlap::Array and call to_burlap on it
+  describe "#to_burlap" do
+    it "should return a string" do
+      [].to_burlap.should be_a_kind_of(String)
+    end
+    it "should create a Burlap::Array instance from itself and delegate #to_burlap to it" do
+      @array = ["foo", :bar]
+
+      mock_burlap_array = mock(Burlap::Array)
+      Burlap::Array.should_receive(:[]).with(*@array).and_return(mock_burlap_array)
+      mock_burlap_array.should_receive(:to_burlap).and_return("<burlap>array</burlap>")
+
+      @array.to_burlap.should == "<burlap>array</burlap>"
+    end
+  end
 end
