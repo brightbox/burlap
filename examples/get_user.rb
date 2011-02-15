2e458209cc333ea30e86063dabbaa741bb83bbc6
Caius Durling <dev@caius.name>
2011-02-15 17:32:50 +0000

    Stripping examples out for now

--- examples/get_user.rb
+++
@@ -1,8 +0,0 @@
-require "rubygems"
-require "bundler/setup"
-require "burlap"
-require "pp"
-
-file = File.expand_path("getUserWs.xml", File.dirname(__FILE__))
-datafile = File.new(file)
-pp Burlap.parse(datafile)
