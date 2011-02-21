bfe96659242c407a191b07b3e09904fdc8da7d3e
Caius Durling <dev@caius.name>
2011-02-21 12:32:15 +0000

    Adding specs for default resolver parsing tags

--- spec/burlap/default_resolver_spec.rb
+++ spec/burlap/default_resolver_spec.rb
@@ -1,9 +1,9 @@
 require "spec_helper"
 
 describe Burlap::DefaultResolver do
-  before { @resolver = Burlap::DefaultResolver.new }
 
   describe "#mappings" do
+    before { @resolver = Burlap::DefaultResolver.new }
     it "should default to a hash" do
       @resolver.mappings.should == {}
     end
@@ -39,6 +39,26 @@ describe Burlap::DefaultResolver do
       end
     end
     it "should use the last argument if it responds to #call in place of a block"
+  end
+
+  describe "parsing default tags" do
+    before(:all) do
+      @resolver = Burlap.resolver
+    end
+
+    [
+      {:name => "int", :burlap => "15", :expected => 15},
+      {:name => "string", :burlap => "some string value", :expected => "some string value"},
+      {:name => "null", :burlap => "", :expected => nil}
+    ].each do |tag|
+      describe tag[:name] do
+        it "should be parsed into ruby" do
+          burlap_tag = Burlap::BaseTag.new(:name => tag[:name], :value => tag[:burlap])
+          @resolver.convert_to_native(burlap_tag).should == tag[:expected]
+        end
+      end
+    end
+
   end
 
 end
