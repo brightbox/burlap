a5b968afc39f57f57739b2ff815e06e9a15b78b5
Caius Durling <dev@caius.name>
2011-03-29 14:29:42 +0100

    Making use of Time#burlap_iso8601

--- lib/burlap/core_ext/time.rb
+++ lib/burlap/core_ext/time.rb
@@ -1,7 +1,5 @@
-require "time"
-
 class Time
   def to_burlap
-    Burlap::Node.new(:name => "date", :value => self.iso8601(3)).to_burlap
+    Burlap::Node.new(:name => "date", :value => self.burlap_iso8601(3)).to_burlap
   end
 end
--- spec/burlap/core_ext/time_spec.rb
+++ spec/burlap/core_ext/time_spec.rb
@@ -3,13 +3,19 @@ require "spec_helper"
 describe Time do
   describe "#to_burlap" do
     before do
-      @result = Time.local(2011, 9, 11, 10, 0).to_burlap
+      @result = Time.utc(2011, 9, 11, 10, 0).to_burlap
     end
     it "should return a string" do
       @result.should be_a_kind_of(String)
     end
     it "should be correct" do
-      @result.should == "<date>#{Time.local(2011, 9, 11, 10, 0).iso8601(3)}</date>"
+      @result.should == "<date>20110911T100000.000Z</date>"
+    end
+    it "should not contain dashes" do
+      @result.should_not =~ /-/
+    end
+    it "should not contain colons" do
+      @result.should_not =~ /:/
     end
   end
 end
