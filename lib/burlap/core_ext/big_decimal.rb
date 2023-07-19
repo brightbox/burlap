6f68bbf38d18a690df9e541c28c25c1163331065
Paul Thornthwaite <paul@brightbox.com>
2023-07-19 16:58:58 +0100

    Remove extra compatibilty files
    To avoid deprecation warnings that are only relevant when using
    `Fixnum`, we can remove special case behaviour and just use the
    `Integer` class available to all the Ruby versions we can support.

--- lib/burlap/core_ext/big_decimal.rb
+++
@@ -1,8 +0,0 @@
-#require "big_decimal"
-
-class BigDecimal
-  def to_burlap
-    # <map><type>java.math.BigDecimal</type><string>value</string><string>0</string></map>
-    Burlap::Hash[{:value => to_i.to_s}, "java.math.BigDecimal"].to_burlap
-  end
-end
--- lib/burlap/core_ext/integer.rb
+++ lib/burlap/core_ext/integer.rb
@@ -1,12 +1,10 @@
-if 1.class == Integer
-  class Integer
-    def to_burlap
-      if between?(-2**31, 2**31-1)
-        Burlap::Node.new(:name => "int", :value => self.to_i).to_burlap
-      else
-        # <map><type>java.math.BigDecimal</type><string>value</string><string>0</string></map>
-        Burlap::Hash[{:value => self.to_i.to_s}, "java.math.BigDecimal"].to_burlap
-      end
+class Integer
+  def to_burlap
+    if between?(-2**31, 2**31-1)
+      Burlap::Node.new(:name => "int", :value => self.to_i).to_burlap
+    else
+      # <map><type>java.math.BigDecimal</type><string>value</string><string>0</string></map>
+      Burlap::Hash[{:value => self.to_i.to_s}, "java.math.BigDecimal"].to_burlap
     end
   end
 end
