1c5d74bc4e60d2d885116f90e59a81b3decfaa50
Paul Thornthwaite <paul@brightbox.com>
2023-07-19 16:58:58 +0100

    Remove extra compatibilty files
    To avoid deprecation warnings that are only relevant when using
    `Fixnum`, we can remove special case behaviour and just use the
    `Integer` class available to all the Ruby versions we can support.

--- lib/burlap/core_ext.rb
+++ lib/burlap/core_ext.rb
@@ -1,13 +1,8 @@
 require "burlap/core_ext/array"
 require "burlap/core_ext/boolean"
-if 1.class == Integer
-  require "burlap/core_ext/integer"
-else
-  # require "burlap/core_ext/big_decimal"
-  require "burlap/core_ext/fixnum"
-end
 require "burlap/core_ext/float"
 require "burlap/core_ext/hash"
+require "burlap/core_ext/integer"
 require "burlap/core_ext/nil"
 require "burlap/core_ext/object"
 require "burlap/core_ext/string"
--- lib/burlap/core_ext/fixnum.rb
+++
@@ -1,5 +0,0 @@
-class Fixnum
-  def to_burlap
-    Burlap::Node.new(:name => "int", :value => self).to_burlap
-  end
-end
